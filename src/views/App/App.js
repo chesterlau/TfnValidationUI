3fd739ccb06bea28459fef25aeeb0ad0ee262947
Chester Lau <chesterlau@live.com>
2019-09-28 18:37:04 +1000

    Modified folder structures and code

---
+++ src/views/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ValidateTfn from '../ValidateTfn/ValidateTfn'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <ValidateTfn />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/views/ValidateTfn/ValidateTfn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ValidateTfn.css';
+
+const ValidateTfn = () => {
+  return (
+    <div className="row validate-div">
+      <div className="col-6">
+        <h2>Test</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ValidateTfn;
