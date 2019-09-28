c1ba2c4e8353f68fee148eb1e391bd16e7e7d983
Chester Lau <chesterlau@live.com>
2019-09-28 22:13:42 +1000

    Added logic to validate TFN

--- src/views/ValidateTfn/ValidateTfn.js
+++ src/views/ValidateTfn/ValidateTfn.js
@@ -1,11 +1,58 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import uuid from 'uuid';
 import './ValidateTfn.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ValidateTfn = () => {
+
+  const state = { tfnNumber: "" };
+  const xtraceId = uuid();
+  const handleTextChange = (event) => {
+    state.tfnNumber = event.target.value;
+  };
+
+  const notify = (result) => {
+
+    if (result.isValid) {
+      toast.success(`${result.message}`, {
+        position: toast.POSITION.TOP_CENTER
+      });
+    }
+    else {
+      toast.error(`${result.message}`, {
+        position: toast.POSITION.TOP_CENTER
+      });
+    }
+
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const headers = {
+      "X-trace-ID": xtraceId
+    }    
+
+    axios.post(`http://localhost:5000/api/tfn/validate?tfnNumber=${state.tfnNumber}`, null, {headers: headers})
+      .then(res => {        
+        let validationResult = res.data.validationResult;
+        notify(validationResult);
+      });
+  }
+
   return (
     <div className="row validate-div">
-      <div className="col-6">
-        <h2>Test</h2>
+      <div className="col-9">
+        <ToastContainer autoClose={4000}/>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="TfnNumberLabel">TFN Number</label>
+            <input type="text" className="form-control" onChange={handleTextChange} />
+          </div>
+          <button type="submit" className="btn btn-primary">Validate</button>
+        </form>
       </div>
     </div>
   );
