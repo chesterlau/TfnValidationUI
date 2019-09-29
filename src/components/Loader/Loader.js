eda7bf4961c2114434c7410a2adfb5a666800bd6
Chester Lau <chesterlau@live.com>
2019-09-29 19:58:50 +1000

    Removed more console logging

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,17 +3,13 @@ import './Loader.css';
 
 const Loader = (props) => {
 
-  console.log(props.showLoader);
-
-  if(props.showLoader)
-  {
+  if (props.showLoader) {
     return (
       <div className="loading">Loading&#8230;</div>
     );
   }
-  else
-  {
-    return(null);
+  else {
+    return (null);
   }
 };
 
